ff26241a0cdec5a1a9631f453e82374d3937c651
formation <hei-hong.traing@egis.fr>
2019-09-14 22:17:37 +0200

    class transaction

---
+++ app/domain/transaction/transaction.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Transaction{
+    constructor(transaction, account){
+        this.transaction = transaction,
+        this.account = account;
+    }
+
+    getTransaction(){
+        return this.transaction;
+    }
+
+    getAccount() {
+        return this.account;
+    }
+
+
+}
+
+module.exports = Transaction;
