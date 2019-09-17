bf8d068880cdf6c32ea62a85f5d504ecdbc369cd
hei-hongtraing <traingheihong@gmail.com>
2019-09-17 15:51:25 +0200

    refactoring code

--- app/domain/account/account.js
+++ app/domain/account/account.js
@@ -4,17 +4,21 @@ class Account{
 
     constructor(nameClient) {
         this.nameClient = nameClient || "";
-        this._amount  = amount || 0;
+        this.amount = 0;
     }
 
     getNameClient() {
         return this.nameClient;
     }
 
-    getAmount() {
-        return  this.amount;
-
+    getAmount(){
+        return this.amount;
     }
+
+    setAmount(amount){
+        this.amount = amount;
+    }
+
 }
 
 module.exports = Account;
--- app/domain/statement/statement.js
+++ app/domain/statement/statement.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const Deposit = require('../deposit/deposit');
+const Deposit = "deposit";
 
 
 class Statement{
-    constructor(operation){
-        this.operation = operation;
+    constructor(){
+        this.statements = [];
+    }
+
+    setStatements(operation, balance){
+        if(operation.getTypeOperation() == "deposit"){
+            this.statements += `Deposit ${operation.getAmount()} the ${new Date()} and the balance is ${balance} \n`;
+        }
+        else{
+            this.statements +=`Withdrawal ${operation.getAmount()} the ${new Date()} and the balance is ${balance} \n`;
+        }
+
+    }
+
+    getStatements(){
+        return this.statements;
     }
 }
 
